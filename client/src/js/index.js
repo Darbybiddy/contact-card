8d4775e2a4778176d3f454e8c148067352a7842e
Darby Biddy <darbybiddy3@gmail.com>
2022-12-23 03:22:45 -0600

    added the webpack

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,3 +1,23 @@
-import './form';
-import './submit';
+//import modules
+import './form'
+import './submit'
+
+//import css files
+import '../../src/css/index.css'
+
+//import bootstrap modules
+import {Tooltip, Toast, Popover} from 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+//import images
+import Logo from '../images/logo.png'
+import Bear from '../images/bear.png'
+import Dog from '../images/dog.png'
+
+//add images on load
+window.addEventListener('load', function(){
+    document.getElementById('logo').src = Logo
+    document.getElementById('bearThumbnail').src = Bear
+    document.getElementById('dogThumbnail').src = Dog
+})
 
